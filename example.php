fa7037705647f6d91733e11418d86da8767de6b9
Dale Horton <viper7@viper-7.com>
2012-11-19 07:47:08 +1100

    Updated Example (again)

--- example.php
+++ example.php
@@ -4,21 +4,19 @@ include 'PSO.php';
 $pool = new PSO_TCPServerPool();
 $pool->openPort(8004);
 $pool->onData(function($data) {
-	$this->pool->broadcast($data);
+	$this->send($data);
 });
 
 
 $clientPool = new PSO_TCPClientPool();
-
+$clientPool->addTarget('localhost', '8004');
 $clientPool->onData(function($data) {
 	var_dump($data);
 });
-
 $clientPool->onTick(function() { 
 	$this->broadcast(microtime(true));
 });
 
-$conn1 = $clientPool->addTarget('localhost', '8004');
 
 
 PSO::drain($pool, $clientPool);
